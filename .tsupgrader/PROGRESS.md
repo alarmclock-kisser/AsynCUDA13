0858aa755409d99b748d8dfb23eb52f46d73e1de
Marcel King <marcel.king91299@gmail.com>
2026-08-24 15:02:30 +0200

    ***

--- .tsupgrader/PROGRESS.md
+++
@@ -1,6 +0,0 @@
-﻿[Information] [Updater] # TypeScript Package Updater
-
-**Analyzing repository:** `C:\Users\marce\source\repos\AsynCUDA13.Runtime`
-
-[Information] [Updater] 📦 **Found 0 package installation tasks**
-
--- tmp/zen1.ps1
+++
@@ -1,9 +0,0 @@
-$dll = "$env:USERPROFILE\.nuget\packages\radzen.blazor\11.2.5\lib\net10.0\Radzen.Blazor.dll"
-$asm = [System.Reflection.Assembly]::LoadFrom($dll)
-$type = $asm.GetType('Radzen.Blazor.RadzenDialog')
-Write-Host "Type found: $($null -ne $type)"
-$props = $type.GetProperty([string[]]@('Visible','VisibleChanged','Width','Height','Title','CloseOnEsc','RenderMode','DialogStyle','FullWidth','ShowCloseButton','Class','Style'))
-foreach($p in $props){ if($p){ Write-Host ("{0} : {1}" -f $p.Name, $p.PropertyType.Name) } }
-Write-Host "--- ALL PUBLIC PARAMETER-PROPERTIES on RadzenDialog ---"
-$props = $type.GetProperties([System.Reflection.BindingFlags]::Public -bor [System.Reflection.BindingFlags]::Instance) | Where-Object { $_.GetCustomAttribute([Microsoft.AspNetCore.Components.ParameterAttribute]) -ne $null }
-$props | ForEach-Object { Write-Host $_.Name }
